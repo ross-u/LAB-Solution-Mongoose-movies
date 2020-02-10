b4adaf4249ec400d508c1316db0d7f0b5367bd7a
Uros Cirkovic <cirkovic.uros.srb@gmail.com>
2020-02-10 21:29:56 +0100

    Update app.js

--- starter-code/app.js
+++ starter-code/app.js
@@ -6,7 +6,7 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const hbs = require('hbs');
 
-const index = require('./routes/index');
+const router = require('./routes/index');
 
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/mongoose-movies-development', {
@@ -29,7 +29,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
-app.use('/', index);
+app.use('/', router);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
